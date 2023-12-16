45efee74ee2a78289ca4b2f78358fac63b71aba7
tayboy217 <tayboy217@icloud.com>
2023-12-17 02:16:35 +0900

    Update Spring project

---
+++ src/main/java/com/english/cafe/EnglishCafeApplication.java
@@ -0,0 +1,13 @@
+package com.english.cafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishCafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishCafeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/english/cafe/EnglishCafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.english.cafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishCafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
